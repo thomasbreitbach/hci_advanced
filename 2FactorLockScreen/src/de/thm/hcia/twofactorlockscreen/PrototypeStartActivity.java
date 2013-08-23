1c8b53c54936204d1c08b29deae173f5af415db8
thomasbreitbach <thomas.breitbach@iem.thm.de>
2013-08-23 16:21:00 +0200

    PrototypStart in PrototypStartActivity umbenannt

--- 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/PrototypeStartActivity.java
+++ 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/PrototypeStartActivity.java
@@ -9,7 +9,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class PrototypeStart extends SherlockActivity {
+public class PrototypeStartActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {

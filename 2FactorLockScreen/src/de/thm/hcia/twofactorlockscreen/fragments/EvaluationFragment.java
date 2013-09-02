34ac430e3f80f04234b2a5a330c50319920ac027
thomasbreitbach <Thomas.Breitbach@iem.thm.de>
2013-09-02 15:13:22 +0200

    Progressbar beim laden der webview (Prototyp evaluieren)

--- 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/fragments/EvaluationFragment.java
+++ 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/fragments/EvaluationFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
@@ -16,12 +17,14 @@ public class EvaluationFragment extends SherlockFragment {
 
 	private static Context mContext;
 	private static String url = "http://goo.gl/UqwY7W";
+	private ProgressBar mProgressBar;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		mContext = getActivity();
 		View v = inflater.inflate(R.layout.evaluation_fragment, null); 
 		WebView wv = (WebView) v.findViewById(R.id.webView1);
+		mProgressBar = (ProgressBar) v.findViewById(R.id.loadingURL);
 		
 		wv.getSettings().setJavaScriptEnabled(true);
 		// By default, redirects cause jump from WebView to default
@@ -32,6 +35,12 @@ public class EvaluationFragment extends SherlockFragment {
 	        public boolean shouldOverrideUrlLoading(WebView view, String url) {
 	            view.loadUrl(url);
 	            return false;
+	        }
+	        
+	        @Override
+	        public void onPageFinished(WebView view, String url) {
+	        	super.onPageFinished(view, url);
+	        	mProgressBar.setVisibility(View.GONE);
 	        }
 	    });
 		wv.loadUrl(url);

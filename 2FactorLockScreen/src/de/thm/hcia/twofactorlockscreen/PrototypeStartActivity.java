bdfc01bdb8f4b371581993d69f4105028fdabcb1
wissner <wissner@172.24.190.230>
2013-09-10 10:44:26 +0200

    added the PrptotypeTestActivity

--- 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/PrototypeStartActivity.java
+++ 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/PrototypeStartActivity.java
@@ -1,32 +1,33 @@
 package de.thm.hcia.twofactorlockscreen;
 
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.MenuItem;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.actionbarsherlock.app.SherlockActivity;
 
 public class PrototypeStartActivity extends SherlockActivity {
-
+	private Button nextTest;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.app_name);
 		setContentView(R.layout.prototype_start_activity);
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-	
+		
+		nextTest = (Button) findViewById(R.id.bttnPtest);
+		nextTest.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+	            intent.setClass(getBaseContext(), PrototypeTestActivity.class);
+	            startActivity(intent);
+				
+			}
+		});
+		
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			finish();
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 }

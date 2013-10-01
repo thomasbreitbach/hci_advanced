4c994c64a241cc163767c60700c56dc854683e72
thomasbreitbach <Thomas.Breitbach@iem.thm.de>
2013-10-01 16:56:00 +0200

    tweak

--- 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/fragments/AboutFragment.java
+++ 2FactorLockScreen/src/de/thm/hcia/twofactorlockscreen/fragments/AboutFragment.java
@@ -1,12 +1,15 @@
 package de.thm.hcia.twofactorlockscreen.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +18,7 @@ import com.actionbarsherlock.app.SherlockFragment;
 import de.thm.hcia.twofactorlockscreen.MainActivity;
 import de.thm.hcia.twofactorlockscreen.R;
 
-public class AboutFragment extends SherlockFragment {
+public class AboutFragment extends SherlockFragment implements OnClickListener {
 
 	private static Context mContext;
 
@@ -30,9 +33,23 @@ public class AboutFragment extends SherlockFragment {
 		TextView tvAppVersion = (TextView) v.findViewById(R.id.tv_app_version);
 		tvAppVersion.setText(versionName.toString());
 		
+		ImageButton github = (ImageButton) v.findViewById(R.id.githubLogo);
+		github.setOnClickListener(this);
 		
+		TextView githubtxt = (TextView) v.findViewById(R.id.githubtxt);
+		githubtxt.setOnClickListener(this);
 		
 		return v;
+	}
+	
+	public void onClick(View v){
+		if (v.getId() == R.id.githubLogo || v.getId() == R.id.githubtxt){
+			// Open Browser
+			Intent browserIntent = new Intent(
+					Intent.ACTION_VIEW,
+					Uri.parse("https://github.com/thomasbreitbach/hci_advanced/"));
+			startActivity(browserIntent);
+		}
 	}
 	
 	@Override
